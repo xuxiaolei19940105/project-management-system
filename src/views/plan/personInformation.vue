41d079eab9a58229fc8abcfa03693a2a1b80e440
xxl <876171457@qq.com>
2020-04-09 09:45:07 +0800

    页面上传

---
+++ src/views/plan/personInformation.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="person-information">人员信息</div>
+</template>
+
+<script>
+export default {
+    name: 'person-information'
+};
+</script>
---
+++ src/views/plan/project.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="project">项目信息</div>
+</template>
+
+<script>
+export default {
+    name: 'project'
+};
+</script>
