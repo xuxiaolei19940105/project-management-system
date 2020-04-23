2877e34fd2b614965014da7d8f321e5cf7dabacf
xuxiaolei <876171457@qq.com>
2020-04-23 19:56:49 +0800

    日志界面数据处理

--- src/components/journal.vue
+++ src/components/journal.vue
@@ -14,9 +14,13 @@
                             height="250"
                             size="mini"
                         >
-                            <el-table-column prop="content" label="内容"></el-table-column>
-                            <el-table-column prop="name" label="人员" width="180"></el-table-column>
-                            <el-table-column prop="time" label="时间"></el-table-column>
+                            <el-table-column prop="operationDescribe" label="内容"></el-table-column>
+                            <el-table-column prop="operationUserName" label="人员" width="180"></el-table-column>
+                            <el-table-column prop="inserttime" label="时间">
+                                <template slot-scope="scope">
+                                    <span>{{ scope.row.inserttime.slice(0, 10)}}</span><span>&nbsp;</span><span>{{ scope.row.inserttime.slice(11, 19)}}</span>
+                                </template>
+                            </el-table-column>
                         </el-table>
                     </div>
                 </el-col>
@@ -26,6 +30,15 @@
 </template>
 <script>
 export default {
+    created() {
+        let pro_id = localStorage.getItem('pro_id');
+        let projectObjectId = {};
+        projectObjectId.id = pro_id;
+        this.$api.task.initProData(projectObjectId).then(response => {
+            console.log(response);
+            this.tableData = response.data.logList;
+        });
+    },
     data() {
         return {
             tableData: [
@@ -66,5 +79,4 @@ export default {
 };
 </script>  
 <style scoped>
-
 </style>
